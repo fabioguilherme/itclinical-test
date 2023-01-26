48e3e74f10d7e9ced3e988aa55cc704be64db16c
DESKTOP-RIMA8UN\fabio <fabioguilherme1989@gmail.com>
2023-01-26 18:40:42 +0000

    NOJIRA  AllUpperCaseValidator now uses regex to validate the input char

--- itClinicalTest/src/main/java/org/itclinicaltest/validators/AllUpperCaseValidator.java
+++ itClinicalTest/src/main/java/org/itclinicaltest/validators/AllUpperCaseValidator.java
@@ -5,8 +5,7 @@ import org.itclinicaltest.models.textprocessor.ProcessType;
 public class AllUpperCaseValidator implements Validator{
     @Override
     public boolean isValid(char letter, int position, int nInput) {
-        //TODO maybe use regex here to
-        return Character.isUpperCase(letter) && ((position + 1) % nInput == 0);
+        return Character.valueOf(letter).toString().matches("[A-Z]") && ((position + 1) % nInput == 0);
     }
 
     @Override
