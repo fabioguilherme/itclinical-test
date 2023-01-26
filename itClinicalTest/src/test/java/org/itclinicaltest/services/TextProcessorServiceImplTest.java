68bf56044e4d5e09e021a88b9a7579a63233d748
DESKTOP-RIMA8UN\fabio <fabioguilherme1989@gmail.com>
2023-01-26 17:50:26 +0000

    NOJIRA added the example's tests to the TextProcessorServiceImplTest class

--- itClinicalTest/src/test/java/org/itclinicaltest/services/TextProcessorServiceImplTest.java
+++ itClinicalTest/src/test/java/org/itclinicaltest/services/TextProcessorServiceImplTest.java
@@ -74,4 +74,86 @@ public class TextProcessorServiceImplTest {
         assertThat(response.getLettersGroup()).hasSize(1);
         assertThat(response.getLettersGroup()).contains(entryExcepted);
     }
+
+    @Test
+    public void shouldReturnAProcessedStringUsingN1OnlyUpperCaseTypeAndITCLiNicAlStringAsInputs() {
+        // Given
+        String inputString = "ITCLiNicAl";
+        int nInput = 1;
+        ProcessType typeInput = ProcessType.ONLY_UPPERCASE;
+
+        // When
+        TextProcessorResponse response = this.service.processText(inputString, nInput, typeInput);
+
+        // Then
+        String expectedString = "ITCLNA";
+        MapEntry<Character,Integer> entryExceptedI = MapEntry.entry('I',1);
+        MapEntry<Character,Integer> entryExceptedT = MapEntry.entry('T',1);
+        MapEntry<Character,Integer> entryExceptedC = MapEntry.entry('C',1);
+        MapEntry<Character,Integer> entryExceptedL = MapEntry.entry('L',1);
+        MapEntry<Character,Integer> entryExceptedN = MapEntry.entry('N',1);
+        MapEntry<Character,Integer> entryExceptedA = MapEntry.entry('A',1);
+
+        assertThat(response).isNotNull();
+        assertThat(response.getOutputString()).isNotEmpty();
+        assertThat(response.getOutputString()).isEqualTo(expectedString);
+
+        assertThat(response.getLettersGroup()).isNotNull();
+
+        assertThat(response.getLettersGroup()).hasSize(6);
+        assertThat(response.getLettersGroup()).contains(entryExceptedI, entryExceptedT, entryExceptedC, entryExceptedL, entryExceptedN,entryExceptedA);
+    }
+
+    @Test
+    public void shouldReturnAProcessedStringUsingN2OnlyUpperCaseTypeAndITCLiNicAlStringAsInputs() {
+        // Given
+        String inputString = "ITCLiNicAl";
+        int nInput = 2;
+        ProcessType typeInput = ProcessType.ONLY_UPPERCASE;
+
+        // When
+        TextProcessorResponse response = this.service.processText(inputString, nInput, typeInput);
+
+        // Then
+        String expectedString = "TLN";
+
+        MapEntry<Character,Integer> entryExceptedT = MapEntry.entry('T',1);
+        MapEntry<Character,Integer> entryExceptedL = MapEntry.entry('L',1);
+        MapEntry<Character,Integer> entryExceptedN = MapEntry.entry('N',1);
+
+        assertThat(response).isNotNull();
+        assertThat(response.getOutputString()).isNotEmpty();
+        assertThat(response.getOutputString()).isEqualTo(expectedString);
+
+        assertThat(response.getLettersGroup()).isNotNull();
+
+        assertThat(response.getLettersGroup()).hasSize(3);
+        assertThat(response.getLettersGroup()).contains(entryExceptedT, entryExceptedL, entryExceptedN);
+    }
+
+    @Test
+    public void shouldReturnAProcessedStringUsingN3OnlyUpperCaseTypeAndITCLiNicAlStringAsInputs() {
+        // Given
+        String inputString = "ITCLiNicAl";
+        int nInput = 3;
+        ProcessType typeInput = ProcessType.ONLY_UPPERCASE;
+
+        // When
+        TextProcessorResponse response = this.service.processText(inputString, nInput, typeInput);
+
+        // Then
+        String expectedString = "CNA";
+        MapEntry<Character,Integer> entryExceptedC = MapEntry.entry('C',1);
+        MapEntry<Character,Integer> entryExceptedN = MapEntry.entry('N',1);
+        MapEntry<Character,Integer> entryExceptedA = MapEntry.entry('A',1);
+
+        assertThat(response).isNotNull();
+        assertThat(response.getOutputString()).isNotEmpty();
+        assertThat(response.getOutputString()).isEqualTo(expectedString);
+
+        assertThat(response.getLettersGroup()).isNotNull();
+
+        assertThat(response.getLettersGroup()).hasSize(3);
+        assertThat(response.getLettersGroup()).contains(entryExceptedC, entryExceptedN,entryExceptedA);
+    }
 }
