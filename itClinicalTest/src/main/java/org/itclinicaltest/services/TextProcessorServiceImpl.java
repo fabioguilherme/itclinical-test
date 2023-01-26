d4c046762cda25f86ccd561f124820b6e2d9cb63
DESKTOP-RIMA8UN\fabio <fabioguilherme1989@gmail.com>
2023-01-26 14:53:34 +0000

    NOJIRA added a new test, where the processor should process a string ABBnBnnnBBBB with a n equal to 2

--- itClinicalTest/src/main/java/org/itclinicaltest/services/TextProcessorServiceImpl.java
+++ itClinicalTest/src/main/java/org/itclinicaltest/services/TextProcessorServiceImpl.java
@@ -1,11 +1,23 @@
 package org.itclinicaltest.services;
 
 
+import com.sun.deploy.util.StringUtils;
 import org.itclinicaltest.models.textprocessor.ProcessType;
 import org.itclinicaltest.models.textprocessor.TextProcessorResponse;
+import org.itclinicaltest.validators.Validator;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class TextProcessorServiceImpl implements TextProcessorService {
 
+    private final Validator uppercaseValidator;
+
+    public TextProcessorServiceImpl(Validator uppercaseValidator) {
+        this.uppercaseValidator = uppercaseValidator;
+    }
 
     @Override
     public TextProcessorResponse processText(String text, int inputN, ProcessType type) {
@@ -13,7 +25,37 @@ public class TextProcessorServiceImpl implements TextProcessorService {
         if (isInputInvalid(text, inputN))
                 return TextProcessorResponse.Builder.withEmptyResponse().build();
 
-        return null;
+        String processedString = processString(text, inputN);
+        Map<Character, Integer> letterGroups = groupLetters(processedString);
+
+        return TextProcessorResponse.Builder.with().outputString(processedString).lettersGroup(letterGroups).build();
+    }
+
+    private Map<Character, Integer> groupLetters(String processedString) {
+
+        Map<Character, Integer> result = new HashMap<>();
+
+        String countingString = processedString;
+
+        int position = 0;
+
+        while(countingString.length() != 0) {
+
+            position = 0;
+            char target = countingString.charAt(position);
+
+            int numberOfLetters = countingString.replaceAll("[^" + target + "]","").length();
+
+            result.put(Character.valueOf(target), numberOfLetters);
+
+            countingString = countingString.replaceAll("[" + target + "]","");
+        }
+
+        return result;
+    }
+
+    private String processString(String text, int inputN) {
+        return IntStream.range(0, text.length()).filter(position -> uppercaseValidator.isValid(text.charAt(position), position, inputN)).mapToObj(position -> Character.valueOf(text.charAt(position)).toString()).collect(Collectors.joining());
     }
 
 
--- itClinicalTest/src/test/java/org/itclinicaltest/services/TextProcessorServiceImplTest.java
+++ itClinicalTest/src/test/java/org/itclinicaltest/services/TextProcessorServiceImplTest.java
@@ -1,11 +1,14 @@
 package org.itclinicaltest.services;
 
+import org.assertj.core.data.MapEntry;
 import org.itclinicaltest.models.textprocessor.ProcessType;
 import org.itclinicaltest.models.textprocessor.TextProcessorResponse;
+import org.itclinicaltest.validators.AllUpperCaseValidator;
 import org.junit.Before;
 import org.junit.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.entry;
 
 public class TextProcessorServiceImplTest {
 
@@ -13,7 +16,7 @@ public class TextProcessorServiceImplTest {
 
     @Before
     public void setUp() throws Exception {
-        this.service = new TextProcessorServiceImpl();
+        this.service = new TextProcessorServiceImpl(new AllUpperCaseValidator());
     }
 
     @Test
@@ -46,5 +49,29 @@ public class TextProcessorServiceImplTest {
         assertThat(response).isNotNull();
         assertThat(response.getOutputString()).isEmpty();
         assertThat(response.getLettersGroup()).isNull();
+    }
+
+    @Test
+    public void shouldReturnAProcessedStringUsingN2AndOnlyUpperCaseTypeAndAStringAsInputs() {
+        // Given
+        String inputString = "ABBnBnnnBBBB";
+        int nInput = 2;
+        ProcessType typeInput = ProcessType.ONLY_UPPERCASE;
+
+        // When
+        TextProcessorResponse response = this.service.processText(inputString, nInput, typeInput);
+
+        // Then
+        String expectedString = "BBB";
+        MapEntry<Character,Integer> entryExcepted = MapEntry.entry('B',3);
+
+        assertThat(response).isNotNull();
+        assertThat(response.getOutputString()).isNotEmpty();
+        assertThat(response.getOutputString()).isEqualTo(expectedString);
+
+        assertThat(response.getLettersGroup()).isNotNull();
+
+        assertThat(response.getLettersGroup()).hasSize(1);
+        assertThat(response.getLettersGroup()).contains(entryExcepted);
     }
 }
